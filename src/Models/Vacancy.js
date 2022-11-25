c1dcd12ab4797880e9fadf667335a04ecc35fc40
d.rogov <d.rogov@autorus.ru>
2022-11-25 14:44:58 +0300

    добавлены ошибки валидации полей

--- src/Models/Vacancy.js
+++ src/Models/Vacancy.js
@@ -6,9 +6,9 @@ const Vacancy = new mongoose.Schema(
     skills: { type: String, required: true },
     responsibilities: { type: String, required: true },
     description: { type: String, required: true },
-    priority: { type: Number, default: 1 },
+    priority: { type: Number, default: 1, min: [1, 'TooLowPriority'], max: [999, 'TooHighPriority'] },
     active: { type: Boolean, default: true },
-    salary: { type: Number, default: 0 },
+    salary: { type: Number, required: true, min: [1, 'TooLowSalary'], max: [999999, 'TooHighSalary'] },
   },
   {
     timestamps: true,
@@ -18,9 +18,25 @@ const Vacancy = new mongoose.Schema(
 )
 
 Vacancy.post('save', function (error, res, next) {
+  console.log('error: ', JSON.stringify(error))
   if (error.name === 'MongoServerError' && error.code === 11000) {
     // Ошибка контроля уникальности
     next({ message: { status: 'violations', violations: [{ code: error.code, message: 'Не дублируйте название вакансии!' }] } })
+  } else if (error.name === 'ValidationError') {
+    // Ошибка валидации полей
+    if (error.message.includes('TooLowPriority') || error.message.includes('TooHighPriority')) {
+      next({
+        message: { status: 'violations', violations: [{ code: error.code, message: 'Приоритет должен быть в интервале 1-999' }] },
+      })
+    } else if (error.message.includes('TooLowSalary') || error.message.includes('TooHighSalary')) {
+      next({
+        message: { status: 'violations', violations: [{ code: error.code, message: 'Зарплата должна быть в интервале 1-999999' }] },
+      })
+    } else {
+      next({
+        message: { status: 'violations', violations: [{ code: error.code, message: 'Заполните корректно все поля!' }] },
+      })
+    }
   } else if (error.name === 'VersionError') {
     // Ошибка контроля версии
     next({
